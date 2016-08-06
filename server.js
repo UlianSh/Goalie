1222d39e7ca643bd5e594edf00998fd017b1bfec
Mason Brothers <brothers.mason@gmail.com>
2016-08-06 10:43:38 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+var onPort = 3000;
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', function (req, res) {
+    res.send('Post: ' + new Date());
+});
+
+app.listen(onPort, function () {
+    console.log('App Running on Port: ' + onPort);
+});

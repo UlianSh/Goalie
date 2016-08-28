08321d937c1d2ed10b1a53638620d8d191f5be1f
Mason Brothers <brothers.mason@gmail.com>
2016-08-28 14:53:01 -0600

    Added Save function

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,5 @@
+loadTiles();
+
 $.get("/getImagesJSON", function(data, status) {
     files = JSON.parse(data);
     console.log(files);
@@ -6,7 +8,7 @@ $.get("/getImagesJSON", function(data, status) {
     for (var i = 0; i < files.length; i++) 
     {
         document.getElementById("mySidenav").innerHTML += 
-        '<img src=' + files[i][0]+ ' class="tileImage" ' + 'onmouseover=openNav2('+i+') ' + ' onmouseout=closeNav2() ' + '>' + 
+        '<img src=' + files[i][0]+ ' class="tileImage" ' + 'onmouseover=openNav2('+i+') ' + ' onmouseout=closeNav2()>' + 
         '<br>' +
         '<br>'
     }          
@@ -77,9 +79,13 @@ window.onclick = function(event) {
 
     
 function playFunc(childButton){
-    var text = childButton.parentElement.getElementsByClassName("textInput")[0].value;
-    var speech = new SpeechSynthesisUtterance(text);
-    window.speechSynthesis.speak(speech);
+    window.speechSynthesis.cancel();
+    setTimeout(function() {
+        var text = childButton.parentElement.getElementsByClassName("textInput")[0].value;
+        var speech = new SpeechSynthesisUtterance(text);
+        window.speechSynthesis.speak(speech);
+    }, 1000)
+
 }
 
 
@@ -97,8 +103,9 @@ function openNav2(folderNum) {
     document.getElementById("mySidenav2").innerHTML=null;
     for (var ii = 1; ii< files[folderNum].length; ii++) 
     {
+        var imgSrc = files[folderNum][ii];
         document.getElementById("mySidenav2").innerHTML += 
-        '<img src='+files[folderNum][ii]+' class="tileImage"  onclick=createTile('+folderNum+','+ii+') onmouseover=enlargeIcon(this) onmouseout=minimizwIcon(this)> '+
+        '<img src="' + imgSrc + '" class="tileImage"  onclick="createTile(\'' + imgSrc + '\')" onmouseover="enlargeIcon(this)" onmouseout="minimizwIcon(this)"> '+
         '<br>' + 
         '<br>'
     }           	 
@@ -148,39 +155,60 @@ function dragMoveListener (event)
     // keep the dragged position in the data-x/data-y attributes
         x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
         y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+    moveTile(target,x,y);
+	
+}
+
+function moveTile(target,x,y)
+{
     target.style.webkitTransform = target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
     target.setAttribute('data-x', x);
     target.setAttribute('data-y', y);
     target.getAttribute('data-x'), target.getAttribute('data-x')
-	
 }
 
 window.dragMoveListener = dragMoveListener;
 
             
-function createTile(group,underGroup)
+function createTile(imgSrc)
 {
-var tile = document.createElement("div");
-tile.style.width = "200px";
-tile.style.height = "200px";
-tile.style.top += Math.floor(((Math.random()*2-1) * 10+ 400))+'px';
-tile.style.left += Math.floor(((Math.random()*2-1) * 10 + 400))+'px';
-tile.style.background = "blue";
-tile.className = "draggable";
-if(files[group][0] != "img/Arrows/BLUE_arrow.png")
-{
-tile.innerHTML = '<img src='+files[group][underGroup]+ ' class="tileImage">' +
-'<button type="button" class="playButton" onclick="playFunc(this)">Play</button>' + 
-'<button type="button" class="closeButton" onclick="closeFunc(this)">X</button>' + 
-'<input type="text" class="textInput">'
-}
+    var tile = document.createElement("div");
+    tile.style.width = "200px";
+    tile.style.height = "200px";
+    //tile.style.top += Math.floor(((Math.random()*2-1) * 10+ 400))+'px';
+    //tile.style.left += Math.floor(((Math.random()*2-1) * 10 + 400))+'px';
+    tile.style.background = "blue";
+    tile.className = "draggable";
+    console.log('test:',imgSrc.indexOf('arrow'))
+    if(imgSrc.indexOf('arrow') == -1)
+    {
+        tile.innerHTML = '<img src="' + imgSrc + '" class="tileImage">' +
+        '<button type="button" class="playButton" onclick="playFunc(this)">Play</button>' + 
+        '<button type="button" class="closeButton" onclick="closeFunc(this)">X</button>' + 
+        '<input type="text" class="textInput">'
+    }
     else
-        {
-            tile.innerHTML = '<img src='+files[group][underGroup]+ ' class="tileImage">'
-        }
-document.body.appendChild(tile);
-randomlyPlace(tile);
-console.log(files[group][underGroup]);
+    {
+        tile.style.width = "auto";
+        tile.style.height = "auto";
+        tile.innerHTML = '<button type="button" class="closeButton" onclick="closeFunc(this)">X</button>' + 
+        '<img src="' + imgSrc + '" class="tileImage">';
+
+    }
+    document.body.appendChild(tile);
+    //randomlyPlace(tile);
+    console.log(imgSrc);
+    return tile;
+}
+
+function loadTile(loadedTile)
+{
+    var tile = createTile(loadedTile.img);
+    moveTile(tile,loadTile.x,loadedTile.y)
+    tile.getElementsByClassName('textInput')[0].value = loadedTile.text;
+    tile.style.background = loadedTile.color;
+    moveTile(tile,loadedTile.x,loadedTile.y)
+    return tile;
 }
 
 function defaultText(textInput) {
@@ -222,19 +250,19 @@ function loadTiles() {
         tiles = JSON.parse((JSON.parse(data)).data);
         for (var i = 0; i < tiles.length; i++)
         {
+            loadTile(tiles[i]);
             //Load in tiles
         }
         console.log(tiles);
     });
 }
 
-function removeTile(){
+function removeTile()
+{
     document.removeChild()
 }
 
- function randomlyPlace(el)
-    {
-    el.style.top = Math.floor(Math.random()*document.body.clientHeight) +'px';
-    el.style.left = Math.floor(Math.random()*document.body.clientWidth) + 'px';
-  }
-
+function randomlyPlace(target)
+{
+    moveTile(target, Math.floor(Math.random()*document.body.clientHeight), Math.floor(Math.random()*document.body.clientWidth))
+}
